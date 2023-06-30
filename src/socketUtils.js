e8e0317e92c12e8f065dbc90e0ca5b59c1291743
elio-joel <elio22arg@gmail.com>
2023-06-29 21:57:14 -0300

    desafio-entregable

--- src/socketUtils.js
+++
@@ -1,9 +0,0 @@
-import { ProductManager } from './manager/productManager.js'
-
-const productsUpdated = async (io) => {
-    const productManager = new ProductManager('./src/data/products.json');
-    const products = await productManager.getProducts();
-    io.emit('productsUpdated', products);    
-};
-
-export { productsUpdated };
